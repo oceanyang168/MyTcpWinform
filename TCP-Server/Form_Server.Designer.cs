3ce2bd4afd0360eb2aeb2004e42a1f01c0fdcf6c
oceanhuang <35588425+oceanyang168@users.noreply.github.com>
2022-09-08 15:53:58 +0800

    Server
    Server

---
+++ TCP-Server/Form_Server.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace TCP_Server
+{
+    partial class Form_Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_message = new System.Windows.Forms.ListBox();
+            this.btn_clearmessage = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.btn_listen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lb_message
+            // 
+            this.lb_message.FormattingEnabled = true;
+            this.lb_message.ItemHeight = 12;
+            this.lb_message.Location = new System.Drawing.Point(23, 54);
+            this.lb_message.Name = "lb_message";
+            this.lb_message.Size = new System.Drawing.Size(246, 124);
+            this.lb_message.TabIndex = 11;
+            // 
+            // btn_clearmessage
+            // 
+            this.btn_clearmessage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_clearmessage.AutoSize = true;
+            this.btn_clearmessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clearmessage.Location = new System.Drawing.Point(23, 183);
+            this.btn_clearmessage.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_clearmessage.Name = "btn_clearmessage";
+            this.btn_clearmessage.Size = new System.Drawing.Size(128, 38);
+            this.btn_clearmessage.TabIndex = 10;
+            this.btn_clearmessage.Text = "Clear msge";
+            this.btn_clearmessage.UseVisualStyleBackColor = true;
+            this.btn_clearmessage.Click += new System.EventHandler(this.btn_clearmessage_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(274, 19);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(142, 220);
+            this.listBox1.TabIndex = 9;
+            // 
+            // btn_close
+            // 
+            this.btn_close.AutoSize = true;
+            this.btn_close.Enabled = false;
+            this.btn_close.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_close.Location = new System.Drawing.Point(111, 19);
+            this.btn_close.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(66, 30);
+            this.btn_close.TabIndex = 8;
+            this.btn_close.Text = "Close";
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click_1);
+            // 
+            // btn_listen
+            // 
+            this.btn_listen.AutoSize = true;
+            this.btn_listen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_listen.Location = new System.Drawing.Point(23, 19);
+            this.btn_listen.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_listen.Name = "btn_listen";
+            this.btn_listen.Size = new System.Drawing.Size(75, 30);
+            this.btn_listen.TabIndex = 7;
+            this.btn_listen.Text = "Start";
+            this.btn_listen.UseVisualStyleBackColor = true;
+            this.btn_listen.Click += new System.EventHandler(this.btn_listen_Click);
+            // 
+            // Form_Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 259);
+            this.Controls.Add(this.lb_message);
+            this.Controls.Add(this.btn_clearmessage);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.btn_listen);
+            this.Name = "Form_Server";
+            this.Text = "Form_Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lb_message;
+        private System.Windows.Forms.Button btn_clearmessage;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Button btn_listen;
+    }
+}
---
+++ TCP-Server/Form_Server.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace TCP_Server
+{
+    public partial class Form_Server : Form
+    {
+        Socket socket;
+        Socket client;
+        NetworkStream stream;
+
+        IPAddress ip = IPAddress.Parse("127.0.0.1");
+        int port = 6105;
+        IPEndPoint ipe;
+
+        Thread thread;
+        /// <summary>
+        /// functions
+        /// </summary>
+
+        public delegate void Add_Items(ListBox lb, string data);
+        public void Infor_Message(ListBox lb, string data)
+        {
+            if (lb.InvokeRequired)
+            {
+                Add_Items d = new Add_Items(Infor_Message);
+                this.Invoke(d, new object[] { lb, data });
+            }
+            else
+            {
+                try
+                {
+                    lb.Items.Add(data);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error");
+                }
+            }
+        }
+
+        bool SocketConnected(Socket s)
+        {
+            bool part1 = s.Poll(1000, SelectMode.SelectRead);
+            bool part2 = (s.Available == 0);
+            if (part1 && part2)
+                return false;
+            else
+                return true;
+        }
+
+        public void Server_Thread()
+        {
+            while (true)
+            {
+                client = socket.Accept();
+                listBox1.Items.Add(client.RemoteEndPoint.ToString());
+                stream = new NetworkStream(client);
+                var reader = new StreamReader(stream);
+
+                Infor_Message(lb_message, "New client connected");
+
+                while (SocketConnected(client))
+                {
+
+                    string text = reader.ReadLine();
+                    if (text == string.Empty)
+                    {
+                        Console.WriteLine("text is null");
+                        continue;
+                    }
+
+                    if (SocketConnected(client))
+                    {
+                        Infor_Message(lb_message, "Client: " + text);
+                    }
+                }
+
+                Infor_Message(lb_message, "Client disconnected");
+
+                stream.Close();
+                client.Close();
+            }
+        }
+
+        /// <summary>
+        /// main programing
+        /// </summary>
+        public Form_Server()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_listen_Click(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+
+            ipe = new IPEndPoint(ip, port);
+
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Bind(ipe);
+            socket.Listen(-1);
+
+            thread = new Thread(new ThreadStart(Server_Thread));
+            thread.Start();
+
+            Infor_Message(lb_message, $"Listening port {port}");
+
+            btn_listen.Enabled = false;
+            btn_close.Enabled = true;
+        }
+
+        private void btn_close_Click_1(object sender, EventArgs e)
+        {
+            socket.Close();
+            thread.Abort();
+
+            Infor_Message(lb_message, "Connection closed");
+
+            btn_listen.Enabled = true;
+            btn_close.Enabled = false;
+        }
+
+        private void btn_clearmessage_Click_1(object sender, EventArgs e)
+        {
+            lb_message.Items.Clear();
+        }
+    }
+}
